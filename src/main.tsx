377c2d583c26e636ed73e7dac558eb4932ba4c1f
Cory House <housecor@gmail.com>
2023-01-31 12:42:46 +0100

    add a11y tags

--- src/main.tsx
+++ src/main.tsx
@@ -8,21 +8,25 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   </React.StrictMode>
 );
