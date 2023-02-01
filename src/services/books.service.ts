1b30002e409dec7bdb148229dab21e0958924730
Cory House <housecor@gmail.com>
2023-02-01 10:55:01 +0100

    Use ky

--- src/services/books.service.ts
+++ src/services/books.service.ts
@@ -1,17 +1,9 @@
+import ky from "ky";
 import { Book, bookSchema, NewBook } from "../types/Book.types";
 
 export async function addBook(book: NewBook): Promise<Book> {
-  const resp = await fetch("http://localhost:3001/books", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(book),
-  });
-
-  if (!resp.ok) {
-    throw new Error("Failed to add book");
-  }
-
-  return bookSchema.parse(await resp.json());
+  const resp = await ky
+    .post("http://localhost:3001/books", { json: book })
+    .json();
+  return bookSchema.parse(resp);
 }
