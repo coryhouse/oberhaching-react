55121632f909fd56bc54af58c13e1abd3bffbfa8
Cory House <housecor@gmail.com>
2023-02-01 13:46:24 +0100

    installing reb

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 import App from "./App";
 import "./index.css";
 
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <App />
+  </ErrorBoundary>
 );
