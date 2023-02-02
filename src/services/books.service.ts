23580160d1b31126eaacfbac5209888e6a741b91
Cory House <housecor@gmail.com>
2023-02-02 09:40:04 +0100

    extract getBooks

--- src/services/books.service.ts
+++ src/services/books.service.ts
@@ -1,6 +1,13 @@
 import ky, { ResponsePromise } from "ky";
 import { Book, bookSchema, NewBook } from "../types/Book.types";
 
+export async function getBooks(): Promise<Book[]> {
+  const resp = (await ky
+    .get(import.meta.env.VITE_API_BASE_URL + "/books")
+    .json()) as Book[];
+  return resp.map((book: any) => bookSchema.parse(book));
+}
+
 export async function addBook(book: NewBook): Promise<Book> {
   const resp = await ky
     .post(import.meta.env.VITE_API_BASE_URL + "/books", { json: book })
